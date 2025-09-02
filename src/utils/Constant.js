6d18e04801b59fbaadf4737a96880ab763e8fef6
Vishal Dhanotiya <VDhanotiya@apexanalytix.com>
2025-09-02 15:31:59 +0530

    Change database

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,4 +1,4 @@
-export const isCloudinaryFiles = true;
+export const isCloudinaryFiles = false;
 export const updatedDatabaseKey = isCloudinaryFiles ? 'files':'r2Files';
 
 export const imageTabData = [
